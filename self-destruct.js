9351e8a13f6d41526ea4502357925c8ea56beb37
Bonksy <takyiakwah@outlook.com>
2024-07-03 18:01:48 +0100

    adding self destruct program

---
+++ self-destruct.js
@@ -0,0 +1,8 @@
+
+document.querySelector("h1").textContent = "🔥BOOM!🔥";
+
+alert("Warning! This messeage will self-destruct in");
+alert("3...");
+alert("2...");
+alert("1...");
+console.log("Message destroyed!");
